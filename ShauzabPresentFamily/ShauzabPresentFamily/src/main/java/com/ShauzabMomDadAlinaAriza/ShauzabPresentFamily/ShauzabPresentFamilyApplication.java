08c8b4b822d310a26d12ad52a47e01d0d282e8ec
Shauzab Abbas <42871930+shauzab72abbas@users.noreply.github.com>
2025-09-03 18:40:04 +0530

    Add files via upload

---
+++ ShauzabPresentFamily/ShauzabPresentFamily/src/main/java/com/ShauzabMomDadAlinaAriza/ShauzabPresentFamily/ShauzabPresentFamilyApplication.java
@@ -0,0 +1,13 @@
+package com.ShauzabMomDadAlinaAriza.ShauzabPresentFamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShauzabPresentFamilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShauzabPresentFamilyApplication.class, args);
+	}
+
+}
