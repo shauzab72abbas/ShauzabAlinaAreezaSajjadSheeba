f31031c813c35463bfaa9007927b8a5e6f62b122
Shauzab Abbas <42871930+shauzab72abbas@users.noreply.github.com>
2025-09-13 16:58:28 +0530

    Update ShauzabPresentFamilyApplication.java

--- ShauzabPresentFamily/ShauzabPresentFamily/src/main/java/com/ShauzabMomDadAlinaAriza/ShauzabPresentFamily/ShauzabPresentFamilyApplication.java
+++ ShauzabPresentFamily/ShauzabPresentFamily/src/main/java/com/ShauzabMomDadAlinaAriza/ShauzabPresentFamily/ShauzabPresentFamilyApplication.java
@@ -3,6 +3,7 @@ package com.ShauzabMomDadAlinaAriza.ShauzabPresentFamily;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableScheduling
 @SpringBootApplication
 public class ShauzabPresentFamilyApplication {
 
